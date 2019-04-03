372b30fea94826b10b8166d878ca744d87bda936
Shishmonin Sergey <shishmonin-s@mail.ru>
2019-04-03 13:36:02 +0300

    Первый коммит

---
+++ js/code.js
@@ -0,0 +1,46 @@
+'use strict'
+// alert(22);
+var leftForLife = 44;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+console.log(4/0);
+var string = "Hello world";
+console.log(string);
+let sometime;
+console.log(sometime);
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello world");
+
+// let answer = +prompt("Есть ли Вам 18?", "Yes");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    dect = 10;
+
+
+console.log(incr++);
+console.log(dect--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && isClose);
